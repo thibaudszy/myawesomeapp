faab3354c758b00f52e181db0acf7402de0e0388
thibaudszy <thivaudszy@gmail.com>
2020-10-12 12:22:56 +0200

    saying hello

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
-
+function Hello(){
+  return <h1> Hello </h1>
+}
+function Greeting (props){
+  return <div>
+    <Hello />
+    {props.name}!
+  </div>
+}
 function App() {
   return (
-    <p> hello </p>
-  )
+    <div>
+      <div>
+        <Greeting name='Thibaud' />
+      </div>
+      <div>
+        <Greeting name='yall'/>
+      </div>
+    </div>
+    )
 }
 
 export default App;
